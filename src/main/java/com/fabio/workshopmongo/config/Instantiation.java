91783d3cde09381bca5f5d34fec5f88b8fc71a51
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-01 20:18:58 -0300

    fixing db issues

--- src/main/java/com/fabio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fabio/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fabio.workshopmongo.domain.Post;
 import com.fabio.workshopmongo.domain.User;
+import com.fabio.workshopmongo.dto.AuthorDTO;
 import com.fabio.workshopmongo.repository.PostRepository;
 import com.fabio.workshopmongo.repository.UserRepository;
 
@@ -35,8 +36,10 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Patiu Viagem!" , "Vou viajar para são paulo, abraços.", maria);
-		Post post2 = new Post(null, sdf.parse("21/05/2018"), "Bom dia!" , "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Patiu Viagem!" , "Vou viajar para são paulo, abraços.", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/05/2018"), "Bom dia!" , "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob)); 
