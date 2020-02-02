031b822d97a5897cde2436377d40cf167322d90a
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-01 22:14:30 -0300

    post response working

--- src/main/java/com/fabio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fabio/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fabio.workshopmongo.domain.Post;
 import com.fabio.workshopmongo.domain.User;
 import com.fabio.workshopmongo.dto.UserDto;
 import com.fabio.workshopmongo.services.UserService;
@@ -68,6 +69,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 		
 			
+	}
+	
+	@RequestMapping(value = "/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 
---
+++ src/main/java/com/fabio/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.fabio.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.fabio.workshopmongo.domain.Post;
+
+import com.fabio.workshopmongo.repository.PostRepository;
+import com.fabio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	
+	@Autowired
+	private PostRepository repo;
+		
+	
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}	
+	
+	
+
+}
